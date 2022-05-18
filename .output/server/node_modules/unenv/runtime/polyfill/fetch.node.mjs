b0924f3f256744ce8e788aea137b798a27dc384f
RickySakura <ricky19991018@126.com>
2022-05-18 17:25:47 +0800

    rm

--- .output/server/node_modules/unenv/runtime/polyfill/fetch.node.mjs
+++
@@ -1,6 +0,0 @@
-import * as nodeFetch from "node-fetch";
-import _global from "./globalThis.mjs";
-_global.fetch = _global.fetch || nodeFetch.default || nodeFetch;
-_global.Request = _global.Request || nodeFetch.Request;
-_global.Response = _global.Response || nodeFetch.Response;
-_global.Headers = _global.Headers || nodeFetch.Headers;
